a96684dd6b42256e1e206e12cce1eb50e3ad329d
Abhishekkatale <abhishekkatale427@gmail.com>
2025-05-05 15:26:31 +0530

    push new file

--- src/pages/Razorpay.jsx
+++ src/pages/Razorpay.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import ThankYouMessage from './ThankYouMessage';
+import ThankYouMessage from '../components/ThankYouMessage';
 
 const RazorpayPayment = () => {
     const [name, setName] = useState('');
