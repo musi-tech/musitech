1792110f6b632cbe522d2de16e8de501ad7b9855
Abhishekkatale <abhishekkatale427@gmail.com>
2025-05-13 09:54:29 +0530

    add technova logo in trusted section

--- src/pages/services/InfluencerMarketing.jsx
+++ src/pages/services/InfluencerMarketing.jsx
@@ -19,7 +19,9 @@ import logo9 from '../../assets/images/shree.jpg';
 import logo10 from '../../assets/images/city.JPG'
 import logo11 from '../../assets/images/efent.png'
 import logo12 from '../../assets/images/iyc.jpg'
-import logo13 from '../../assets/images/sm.jpg'
+import logo13 from '../../assets/images/sm.png'
+import logo14 from '../../assets/images/technova.png'
+
 
 const faqs = [
   {
@@ -110,7 +112,7 @@ const InfluencerMarketingPage = () => {
   <h2 className="text-4xl font-bold text-gray-900 mb-8">Trusted By</h2>
   <div className="relative">
     <Slider {...slickSettings} className="py-4">
-      {[logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11, logo12, logo13].map((logo, index) => (
+      {[logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11, logo12, logo13, logo14].map((logo, index) => (
         <div key={index} className="flex-shrink-0 px-4">
           <div className="flex items-center justify-center p-4 bg-white shadow-md rounded-lg">
             <img
